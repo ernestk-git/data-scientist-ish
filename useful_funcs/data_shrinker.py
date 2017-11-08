13e270396be0c5f195da7242c8f8ff92e37510d1
ernestk-git <ekim22@dons.usfca.edu>
2017-11-08 12:39:57 -0800

    Update data_shrinker.py

--- useful_funcs/data_shrinker.py
+++ useful_funcs/data_shrinker.py
@@ -3,8 +3,9 @@ import pandas as pd
 # takes a pandas dataframe and shrinks to smallest datatype
 def data_shrinker(df):
     num_cols = len(train_data.columns)
-    print('Column: %s of %s' % (i, num_cols))
+    
     for i in range(num_cols):
+        print('Column: %s of %s' % (i, num_cols))
         if 'float' in str(df.iloc[:,i].dtype):
             df.iloc[:,i] = pd.to_numeric(df.iloc[:,i], downcast='float')
         elif 'int' in str(df.iloc[:,i].dtype):
